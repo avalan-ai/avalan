d8353d1f38045f2958c134fe155f0250de47e54e
Mariano Iglesias <mgiglesias@gmail.com>
2025-06-11 18:26:30 -0300

    Improving CodeTool (#203)

--- tests/tool/code_tool_test.py
+++ tests/tool/code_tool_test.py
@@ -1,18 +1,21 @@
 from avalan.entities import ToolCallContext
 from avalan.tool.code import CodeTool
-from pytest import importorskip
 from unittest import IsolatedAsyncioTestCase, main
-
-importorskip("RestrictedPython", reason="RestrictedPython not installed")
-
 
 class CodeToolTestCase(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.tool = CodeTool()
 
     async def test_exec(self):
-        result = await self.tool("result = 1 + 1", context=ToolCallContext())
+        result = await self.tool("def run(): return 1 + 1", context=ToolCallContext())
         self.assertEqual(result, "2")
+
+    async def test_exec_with_args(self):
+        result = await self.tool("""
+def run(name: str, stars: int):
+    return f"Hello {name} with {stars} stars!"
+        """, "Leo Messi", context=ToolCallContext(), stars=3)
+        self.assertEqual(result, "Hello Leo Messi with 3 stars!")
 
     async def test_restricted_builtin(self):
         with self.assertRaises(NameError):
