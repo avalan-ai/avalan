4ecde21fffea62f67226e8920a1b50130b83da69
Mariano Iglesias <mgiglesias@gmail.com>
2025-07-08 14:52:05 -0300

    Adding support for voice cloning to TextToSpeechModel

--- docs/examples/audio_text_to_speech.py
+++ docs/examples/audio_text_to_speech.py
@@ -1,33 +1,62 @@
 #
 # EXAMPLE CALL:
 #
-# PYTHONPATH=src poetry run python docs/examples/audio_text_to_speech.py '[S1] Leo Messi (clears throat) is the greatest football player of all times. [S2] Of course he is! (claps)' example.wav
+# rm example.wav ; PYTHONPATH=src poetry run python docs/examples/audio_text_to_speech.py '[S1] Leo Messi (clears throat) is the greatest football player of all times. [S2] Of course he is! (claps)' example.wav
+#
+# EXAMPLE CALL WITH VOICE CLONING:
+#
+# rm example.wav ; PYTHONPATH=src poetry run python docs/examples/audio_text_to_speech.py "[S1] Leo Messi is the greatest football player of all times." example.wav oprah.wav "[S1] And then I grew up and had the esteemed honor of meeting her. And wasn't that a surprise. Here was this petite, almost delicate lady who was the personification of grace and goodness."
+#
 #
 
 from asyncio import run
 from avalan.model.audio import TextToSpeechModel
-import os
-import sys
+from os.path import isfile
+from sys import argv
 
 
-async def example(text: str, path: str) -> None:
+async def example(text: str, path: str, reference_path: str | None, reference_text: str | None) -> None:
     print("Loading model... ", end="", flush=True)
     with TextToSpeechModel("nari-labs/Dia-1.6B-0626") as speech:
-        print(f"DONE. Generating speech for \"{text}\"", flush=True)
+        print(
+            f"DONE. Generating speech for \"{text}\". " + (
+                f"Using {reference_path} as voice reference."
+                if reference_path else ""
+            )
+        , flush=True)
 
-        generated_path = await speech([text], path, max_new_tokens=2560) # corresponds to around ~20s
+        generated_path = await speech(
+            text,
+            path,
+            reference_path=reference_path,
+            reference_text=reference_text,
+            max_new_tokens=5120 # 128 tokens correspond to ~1s
+        )
         print(f"DONE. Speech generated in {generated_path}", flush=True)
 
 
 if __name__ == "__main__":
+    usage = f"Usage: {argv[0]} <text> <file_path> [<reference_path> <reference_text>]"
+
     text = (
-        sys.argv[1]
-        if len(sys.argv) == 3
-        else sys.exit(f"Usage: {sys.argv[0]} <text> <file_path>")
+        argv[1]
+        if len(argv) >= 3
+        else exit(usage)
     )
     path = (
-        sys.argv[2]
-        if len(sys.argv) == 3 and not os.path.isfile(sys.argv[2])
-        else sys.exit(f"Usage: {sys.argv[0]} <text> <file_path>")
+        argv[2]
+        if len(argv) >= 3 and not isfile(argv[2])
+        else exit(usage)
     )
-    run(example(text, path))
+    reference_path = (
+        argv[3]
+        if len(argv) == 5 and isfile(argv[3])
+        else None
+    )
+    reference_text = (
+        argv[4]
+        if len(argv) == 5
+        else None
+    )
+
+    run(example(text, path, reference_path, reference_text))
--- src/avalan/model/audio.py
+++ src/avalan/model/audio.py
@@ -4,7 +4,7 @@ from ..model import TextGenerationVendor, TokenizerNotSupportedException
 from ..model.engine import Engine
 from PIL import Image
 from torch import argmax, inference_mode
-from torchaudio import load
+from torchaudio import functional, load
 from torchaudio.transforms import Resample
 from transformers import (
     AutoProcessor,
@@ -82,6 +82,51 @@ class SpeechRecognitionModel(BaseAudioModel):
         return transcription
 
 
+class SpeechRecognitionModel(BaseAudioModel):
+    def _load_model(self) -> PreTrainedModel | TextGenerationVendor:
+        self._processor = AutoProcessor.from_pretrained(
+            self._model_id,
+            trust_remote_code=self._settings.trust_remote_code,
+            # default behavior in transformers v4.48
+            use_fast=True,
+        )
+        model = AutoModelForCTC.from_pretrained(
+            self._model_id,
+            trust_remote_code=self._settings.trust_remote_code,
+            pad_token_id=self._processor.tokenizer.pad_token_id,
+            ctc_loss_reduction="mean",
+            device_map=self._device,
+            tp_plan=Engine._get_tp_plan(self._settings.parallel),
+        )
+        return model
+
+    @override
+    async def __call__(
+        self,
+        audio_source: str,
+        sampling_rate: int,
+        tensor_format: Literal["pt"] = "pt",
+    ) -> str:
+        audio_wave, original_sampling_rate = load(audio_source)
+        if original_sampling_rate != sampling_rate:
+            resampler = Resample(
+                orig_freq=original_sampling_rate, new_freq=sampling_rate
+            )
+            audio_wave = resampler(audio_wave)
+        audio = audio_wave.squeeze().numpy()
+        inputs = self._processor(
+            audio,
+            sampling_rate=sampling_rate,
+            return_tensors=tensor_format,
+        ).to(self._device)
+        with inference_mode():
+            # shape (batch, time_steps, vocab_size)
+            logits = self._model(inputs.input_values).logits
+        predicted_ids = argmax(logits, dim=-1)
+        transcription = self._processor.batch_decode(predicted_ids)[0]
+        return transcription
+
+
 class TextToSpeechModel(BaseAudioModel):
     def _load_model(self) -> PreTrainedModel | TextGenerationVendor:
         self._processor = AutoProcessor.from_pretrained(
@@ -99,24 +144,58 @@ class TextToSpeechModel(BaseAudioModel):
     @override
     async def __call__(
         self,
-        texts: list[str],
+        prompt: str,
         path: str,
         max_new_tokens: int,
         *,
         padding: bool = True,
+        reference_path: str | None = None,
+        reference_text: str | None = None,
+        sampling_rate: int = 44_100,
         tensor_format: Literal["pt"] = "pt",
     ) -> str:
+        assert (
+            (not reference_path and not reference_text) or
+            (reference_path and reference_text)
+        )
+
+        reference_voice = None
+        if reference_path and reference_text:
+            reference_voice, reference_sampling_rate = load(reference_path)
+            if reference_sampling_rate != sampling_rate:
+                reference_voice = functional.resample(
+                    reference_voice,
+                    reference_sampling_rate,
+                    sampling_rate
+                )
+            reference_voice = reference_voice.mean(0).numpy()
+
+        text = f"{reference_text}\n{prompt}" if reference_voice is not None else prompt
+
         inputs = self._processor(
-            text=texts,
+            text=text,
+            audio=reference_voice,
             padding=padding,
-            return_tensors=tensor_format
+            return_tensors=tensor_format,
+            sampling_rate=sampling_rate
         ).to(self._device)
+
+        prompt_len = self._processor.get_audio_prompt_len(
+            inputs["decoder_attention_mask"]
+        ) if reference_voice is not None else None
+
         with inference_mode():
             outputs = self._model.generate(
                 **inputs,
                 max_new_tokens=max_new_tokens
             )
 
-        outputs = self._processor.batch_decode(outputs)
-        self._processor.save_audio(outputs, path)
+        wave = self._processor.batch_decode(
+            outputs,
+            audio_prompt_len=prompt_len
+        ) if prompt_len and outputs.shape[1] >= prompt_len else self._processor.batch_decode(
+            outputs
+        )
+
+        self._processor.save_audio(wave, path)
         return path
