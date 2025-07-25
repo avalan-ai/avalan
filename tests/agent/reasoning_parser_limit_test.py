5d52a0a64de478f899d7b940603b0d749021c4eb
Mariano Iglesias <mariano@cricava.com>
2025-07-25 19:45:40 -0300

    Add reasoning parser token limit test

---
+++ tests/agent/reasoning_parser_limit_test.py
@@ -0,0 +1,24 @@
+from unittest import IsolatedAsyncioTestCase
+
+from avalan.entities import ReasoningSettings, ReasoningToken
+from avalan.model.response.parsers.reasoning import ReasoningParser
+
+
+class ReasoningParserLimitTestCase(IsolatedAsyncioTestCase):
+    async def test_token_limit_switches_to_plain_output(self) -> None:
+        parser = ReasoningParser(
+            reasoning_settings=ReasoningSettings(max_new_tokens=2)
+        )
+        outputs = []
+        for text in ["<think>", "a", "b", "c", "</think>", "d"]:
+            outputs.extend(await parser.push(text))
+        self.assertIsInstance(outputs[0], ReasoningToken)
+        self.assertEqual(outputs[0].token, "<think>")
+        self.assertIsInstance(outputs[1], ReasoningToken)
+        self.assertEqual(outputs[1].token, "a")
+        self.assertEqual(outputs[2], "b")
+        self.assertEqual(outputs[3], "c")
+        self.assertIsInstance(outputs[4], ReasoningToken)
+        self.assertEqual(outputs[4].token, "</think>")
+        self.assertEqual(outputs[5], "d")
+        self.assertFalse(parser.is_thinking)
