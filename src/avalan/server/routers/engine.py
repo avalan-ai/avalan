107077be4926122670a0d9d1a411c10ea6413801
Mariano Iglesias <mgiglesias@gmail.com>
2025-09-08 14:59:46 -0300

    Refactor engine reload context (#671)

--- src/avalan/server/routers/engine.py
+++ src/avalan/server/routers/engine.py
@@ -1,7 +1,5 @@
 from .. import di_get_logger, di_set
 from ..entities import EngineRequest, OrchestratorContext
-from ...agent.loader import OrchestratorLoader
-from contextlib import AsyncExitStack
 from dataclasses import replace
 from fastapi import APIRouter, Depends, Request
 from logging import Logger
@@ -16,30 +14,15 @@ async def set_engine(
     logger: Logger = Depends(di_get_logger),
 ) -> dict[str, str]:
     """Reload orchestrator with a new engine URI."""
-    stack: AsyncExitStack = request.app.state.stack
+    stack = request.app.state.stack
     await stack.aclose()
     ctx: OrchestratorContext = request.app.state.ctx
-    new_stack = AsyncExitStack()
-    loader = OrchestratorLoader(
-        hub=ctx.hub,
-        logger=logger,
-        participant_id=ctx.participant_id,
-        stack=new_stack,
-    )
+    loader = request.app.state.loader
     if ctx.specs_path:
         orchestrator_cm = await loader.from_file(
             ctx.specs_path,
             agent_id=request.app.state.agent_id,
             uri=engine.uri,
-        )
-        ctx = OrchestratorContext(
-            loader=loader,
-            hub=ctx.hub,
-            participant_id=ctx.participant_id,
-            specs_path=ctx.specs_path,
-            settings=ctx.settings,
-            browser_settings=ctx.browser_settings,
-            database_settings=ctx.database_settings,
         )
     else:
         assert ctx.settings
@@ -50,17 +33,14 @@ async def set_engine(
             database_settings=ctx.database_settings,
         )
         ctx = OrchestratorContext(
-            loader=loader,
-            hub=ctx.hub,
             participant_id=ctx.participant_id,
             specs_path=ctx.specs_path,
             settings=settings,
             browser_settings=ctx.browser_settings,
             database_settings=ctx.database_settings,
         )
-    orchestrator = await new_stack.enter_async_context(orchestrator_cm)
     request.app.state.ctx = ctx
-    request.app.state.stack = new_stack
+    orchestrator = await stack.enter_async_context(orchestrator_cm)
     request.app.state.agent_id = orchestrator.id
     di_set(request.app, logger=logger, orchestrator=orchestrator)
     return {"uri": engine.uri}
