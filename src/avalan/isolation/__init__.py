7aeba454247208adec143bb20456cac15c5df8c1
Mariano Iglesias <mgiglesias@gmail.com>
2026-06-24 23:16:51 -0300

    Add isolation settings

---
+++ src/avalan/isolation/__init__.py
@@ -0,0 +1,90 @@
+from .settings import (
+    IsolationDiagnostic as IsolationDiagnostic,
+)
+from .settings import (
+    IsolationDiagnosticCategory as IsolationDiagnosticCategory,
+)
+from .settings import (
+    IsolationDiagnosticCode as IsolationDiagnosticCode,
+)
+from .settings import (
+    IsolationEffectiveSettings as IsolationEffectiveSettings,
+)
+from .settings import (
+    IsolationMode as IsolationMode,
+)
+from .settings import (
+    IsolationProfileSelection as IsolationProfileSelection,
+)
+from .settings import (
+    IsolationSettings as IsolationSettings,
+)
+from .settings import (
+    IsolationSettingsSource as IsolationSettingsSource,
+)
+from .settings import (
+    IsolationSettingsSurface as IsolationSettingsSurface,
+)
+from .settings import (
+    IsolationToolRuntimeSettings as IsolationToolRuntimeSettings,
+)
+from .settings import (
+    IsolationTrustLevel as IsolationTrustLevel,
+)
+from .settings import (
+    LocalIsolationPolicy as LocalIsolationPolicy,
+)
+from .settings import (
+    SandboxBackend as SandboxBackend,
+)
+from .settings import (
+    SandboxChildProcessPolicy as SandboxChildProcessPolicy,
+)
+from .settings import (
+    SandboxEffectiveSettings as SandboxEffectiveSettings,
+)
+from .settings import (
+    SandboxEnvironmentPolicy as SandboxEnvironmentPolicy,
+)
+from .settings import (
+    SandboxNetworkMode as SandboxNetworkMode,
+)
+from .settings import (
+    SandboxNetworkPolicy as SandboxNetworkPolicy,
+)
+from .settings import (
+    SandboxOutputPolicy as SandboxOutputPolicy,
+)
+from .settings import (
+    SandboxProfile as SandboxProfile,
+)
+from .settings import (
+    SandboxProfileSelection as SandboxProfileSelection,
+)
+from .settings import (
+    SandboxResourceLimits as SandboxResourceLimits,
+)
+from .settings import (
+    SandboxSettings as SandboxSettings,
+)
+from .settings import (
+    deserialize_isolation_effective_settings as deserialize_isolation_effective_settings,  # noqa: E501
+)
+from .settings import (
+    isolation_diagnostic as isolation_diagnostic,
+)
+from .settings import (
+    isolation_selection_from_mapping as isolation_selection_from_mapping,
+)
+from .settings import (
+    serialize_isolation_effective_settings as serialize_isolation_effective_settings,  # noqa: E501
+)
+from .settings import (
+    trusted_isolation_runtime_from_mapping as trusted_isolation_runtime_from_mapping,  # noqa: E501
+)
+from .settings import (
+    trusted_isolation_settings_from_mapping as trusted_isolation_settings_from_mapping,  # noqa: E501
+)
+from .settings import (
+    trusted_isolation_source as trusted_isolation_source,
+)
